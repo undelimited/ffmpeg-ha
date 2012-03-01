df4a7f836ee20198b964c5d979f3b6543d99d8a9
linuxdev <linuxdev@linuxdev-vm.(none)>
2012-03-01 18:52:40 -0500

    replaced abort with av_abort. Implemented callback for changing av_abort behavior.

--- libavcodec/shorten.c
+++ libavcodec/shorten.c
@@ -182,7 +182,7 @@ static void init_offset(ShortenContext *s)
             break;
         default:
             av_log(s->avctx, AV_LOG_ERROR, "unknown audio type");
-            abort();
+            av_abort();
     }
 
     for (chan = 0; chan < s->channels; chan++)
